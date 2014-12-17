fa4107771a6801b041a456f165fd0d2c9360f27d
cycool89 <cycool89@gmail.com>
2014-12-17 13:39:02 +0000

    Kisebb bugfixek
    Kommentelések

--- Development/core/Classes/Loader.php
+++ Development/core/Classes/Loader.php
@@ -10,20 +10,54 @@ namespace core;
 class Loader {
 
   /**
-   *
+   * Betöltött objektumokat tartalmazó tömb.
+   * 
    * @var aClass
    */
   private static $classes = array();
 
-  /** @var aClass[] */
+  /**
+   * Az objektum ami tartalmazza a Loader osztály
+   * ezen példányát.
+   * 
+   * @var aClass 
+   */
   private $parent;
+
+  /**
+   * A modul gyökérkönyvtára ahonnan betölti a modulhoz tartozó osztályokat.
+   * 
+   * @var string 
+   */
   private $incDir = '';
 
+  /**
+   * Loader konstruktora
+   * 
+   * A <var>$parent</var> paraméterben azt az objektumot várja,
+   * amiben példűnyosítva lett. Az <var>$inc_dir</var> paraméterben pedig
+   * a modul gyökérkönyvtárát.
+   * 
+   * @param \core\aClass $parent
+   * @param string $inc_dir
+   */
   public function __construct(aClass &$parent, $inc_dir) {
     $this->parent = $parent;
     $this->incDir = $inc_dir;
   }
 
+  /**
+   * Controller hozzáadása
+   * 
+   * Hozzáad a <var>$parent</var> objektumhoz egy <var>$name</var> mezőt,
+   * ami ezután a <var>$parent</var> objektumban $this->$name -ként lesz elérhető.
+   * A <var>$name</var> osztály egy \core\aController vagy \core\aFromController
+   * leszármazottja kell legyen és a modul gyökeréhez viszonyítva a 
+   * /Controllers/<var>$name</var>.php helyen kell legyen.
+   * 
+   * @param string $name
+   * @return mixed A <var>$name</var> objektum boot() metódusának visszatérési értéke
+   */
   public function controller($name) {
     $bootValue = $this->load($name/* . '_Controller' */, 'Controllers');
     if ($this->parent->$name instanceof aFormController && !is_null(Request::POST(get_class($this->parent->$name)))) {
@@ -40,19 +74,68 @@ class Loader {
     return $bootValue;
   }
 
+  /**
+   * Model hozzáadása
+   * 
+   * Hozzáad a <var>$parent</var> objektumhoz egy <var>$name</var> mezőt,
+   * ami ezután a <var>$parent</var> objektumban $this->$name -ként lesz elérhető.
+   * A <var>$name</var> osztály egy \core\aModel
+   * leszármazottja kell legyen és a modul gyökeréhez viszonyítva a 
+   * /Models/<var>$name</var>.php helyen kell legyen.
+   * 
+   * @param string $name
+   * @return mixed A <var>$name</var> objektum boot() metódusának visszatérési értéke
+   */
   public function model($name) {
     return $this->load($name, 'Models');
   }
 
+  /**
+   * Almodul hozzáadása
+   * 
+   * Hozzáad a <var>$parent</var> objektumhoz egy <var>$name</var> mezőt,
+   * ami ezután a <var>$parent</var> objektumban $this->$name -ként lesz elérhető.
+   * A <var>$name</var> osztály egy \core\aModule
+   * leszármazottja kell legyen és a modul gyökeréhez viszonyítva a 
+   * /Modules/<var>$name</var>/<var>$name</var>.php helyen kell legyen.
+   * 
+   * @param string $name
+   * @return mixed A <var>$name</var> objektum boot() metódusának visszatérési értéke
+   */
   public function submodule($name) {
     return $this->load($name, $this->incDir . 'Modules' . DS . $name . DS, true);
   }
 
+  /**
+   * Modul hozzáadása
+   * 
+   * Hozzáad a <var>$parent</var> objektumhoz egy <var>$name</var> mezőt,
+   * ami ezután a <var>$parent</var> objektumban $this->$name -ként lesz elérhető.
+   * A <var>$name</var> osztály egy \core\aModule
+   * leszármazottja kell legyen és az alkalmazás gyökeréhez viszonyítva a 
+   * /Modules/<var>$name</var>/<var>$name</var>.php helyen kell legyen.
+   * 
+   * @param string $name
+   * @return mixed A <var>$name</var> objektum boot() metódusának visszatérési értéke
+   */
   public function module($name) {
     $path = Config::getEntry('ModuleDirectory') . $name . DS;
     return $this->load($name, $path, true);
   }
 
+  /**
+   * Nézet betöltése
+   * 
+   * A <var>$name</var> paraméter alapján létrehoz egy Smarty_Internal_template objektumot.
+   * A $this->view->getTemplate() metódus visszatérési értékében a
+   * következőképp fog szerepelni:
+   * 
+   * $t = $this->view->getTemplates();
+   * 
+   * $t[Modulenév][$name];
+   * 
+   * @param string $name
+   */
   public function view($name) {
     $view = AE()->getApplication()->getView();
     $module = getClassName($this->parent->getModule());
@@ -60,22 +143,48 @@ class Loader {
     $view->addTemplate($module, $name, $tpl);
   }
 
+  /**
+   * Megadja, hogy a <var>$className</var> osztály példányosítva lett-e.
+   * 
+   * @param string $className
+   * @return boolean
+   */
   public static function isLoaded($className) {
     return (isset(self::$classes[$className]));
   }
 
+  /**
+   * Visszaadja a <var>$className</var> objektumot, ha be volt töltve.
+   * Egyébként null
+   * 
+   * @param string $className
+   * @return mixed
+   */
   public static function getLoadedClass($className) {
     return (self::isLoaded($className) ? self::$classes[$className] : null);
   }
 
+  /**
+   * Megadja, hogy a <var>$moduleName</var> modul létezik-e.
+   * 
+   * @param string $moduleName
+   * @return mixed A module elérési útvonala, egyébként false
+   */
   public static function isModuleExists($moduleName) {
-    
+
     if (file_exists(Config::getEntry('ModuleDirectory') . $moduleName . DS . $moduleName . AE_EXT)) {
       return Config::getEntry('ModuleDirectory') . $moduleName . DS;
     }
     return false;
   }
 
+  /**
+   * Megadja, hogy a <var>$module</var> modul <var>$controller</var> controllere létezik-e.
+   * 
+   * @param string $module
+   * @param string $controller
+   * @return mixed A controller elérési útvonala, egyébként false
+   */
   public static function isControllerExists($module, $controller) {
     $dir = self::isModuleExists($module);
     if (file_exists($dir . 'Controllers' . DS . $controller . AE_EXT)) {
@@ -84,8 +193,17 @@ class Loader {
     return false;
   }
 
+  /**
+   * Megadja, hogy a <var>$module</var> modul <var>$controller</var> controllerének
+   * <var>$method</var> metódusa létezik-e.
+   * 
+   * @param string $module
+   * @param string $controller
+   * @param string $method
+   * @return boolean
+   */
   public static function isMethodExists($module, $controller, $method) {
-    $fullName = "\\Controllers\\".$controller;
+    $fullName = "\\Controllers\\" . $controller;
     $dir = self::isControllerExists($module, $controller);
     if ($dir) {
       require_once $dir;
@@ -145,8 +263,8 @@ class Loader {
         $fullName = end($parts) . '\\' . $name;
       }
       self::$classes[$name] = new $fullName();
-      //View hozzáadása
       if (!(self::$classes[$name] instanceof \core\aModel)) {
+        //View hozzáadása
         $view = AE()->getApplication()->getView();
         self::$classes[$name]->setView($view);
         //Module hozzáadása
